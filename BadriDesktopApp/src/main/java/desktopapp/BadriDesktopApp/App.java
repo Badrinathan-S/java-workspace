b8ea36063e38f20c1143dc9bcbe9f9f02517b90f
Badrinathan-S <badrinathan3@gmail.com>
2021-12-09 11:25:48 +0530

    init

---
+++ BadriDesktopApp/src/main/java/desktopapp/BadriDesktopApp/App.java
@@ -0,0 +1,13 @@
+package desktopapp.BadriDesktopApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!!!" );
+    }
+}
