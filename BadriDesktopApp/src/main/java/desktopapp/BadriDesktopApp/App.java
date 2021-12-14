1834cef46f480bdc4c1958979970a3a8018cc3f1
Badrinathan-S <badrinathan3@gmail.com>
2021-12-14 12:21:13 +0530

    user Input

--- BadriDesktopApp/src/main/java/desktopapp/BadriDesktopApp/App.java
+++ BadriDesktopApp/src/main/java/desktopapp/BadriDesktopApp/App.java
@@ -1,5 +1,7 @@
 package desktopapp.BadriDesktopApp;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scan = new Scanner(System.in);
+        
+        String firstName, lastName, email, password;
+        
+        Long phoneNumber=0L;
+        Integer age;
+        
+        System.out.println("Please enter your First Name:");
+        firstName = scan.nextLine();
+        System.out.println("Please enter your Last Name:");
+        lastName = scan.nextLine();
+        System.out.println("Please enter your email:");
+        email = scan.next();
+        System.out.println("Please enter your Password:");
+        password = scan.next();
+        System.out.println("Please enter your contact number:");
+        phoneNumber = scan.nextLong();
+        System.out.println("Please enter your age:");
+        age = scan.nextInt();
+        
+        System.out.println("Your account have been created as "+email+"in Gmail and OTP has sent to "+phoneNumber);
+        
     }
 }
