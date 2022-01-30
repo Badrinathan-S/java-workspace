764f7784110a9563e9201c60a63bffaa42a29807
Badrinathan-S <badrinathan3@gmail.com>
2022-01-30 23:12:07 +0530

    single

---
+++ BadriDesktopApp/src/main/java/oops/DemoSingle.java
@@ -0,0 +1,47 @@
+package oops;
+
+import java.util.Arrays;
+
+public class DemoSingle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyProcess process=new MyProcess();
+		process.show();
+		System.out.println(process.binarySearch(0, process.yet.length-1, "Ruffalo",process.yet));
+		System.out.println(process.binarySearch(0, process.yet.length-1, "Scarlet",process.yet));
+		System.out.println(process.binarySearch(0, process.yet.length-1, "Evans",process.yet));
+		System.out.println(process.binarySearch(0, process.yet.length-1, "Zendaya",process.yet));
+
+	}
+
+}
+
+class MyPack{
+	String[] yet= {"Downey","Evans","Garfield","Hemsworth","Hiddleston","Holland","Jackman","Pratt","Ruffalo","Scarlet","Tobey"};
+	
+	public void show() {
+		// TODO Auto-generated method stub
+		System.out.println(Arrays.toString(yet));
+	}
+	
+}
+
+
+class MyProcess extends MyPack{
+	public int binarySearch(int start,int end,String data,String[] arr)
+	{
+		if(end>=start)
+		{
+			int mid=(end+start)/2;
+			if(arr[mid].compareTo(data)==0)
+				return mid;
+			else if(arr[mid].compareTo(data)>0)
+				return binarySearch(start, mid, data,arr);
+			else
+				return binarySearch(mid+1, end, data,arr);
+		}
+		else
+			return -1;
+	}
+}
